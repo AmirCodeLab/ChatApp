c13451601f8ad6215632022eb59d11b31b5862da
amir sohail <amir.sohail.1683@gmail.com>
2025-09-15 17:30:21 +0500

    splash ui done

---
+++ app/src/main/java/com/amar/chat/main_feature/presentation/splash_screen/SplashScreen.kt
@@ -0,0 +1,60 @@
+package com.amar.chat.main_feature.presentation.splash_screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.amar.chat.R
+
+@Composable
+@Preview(showSystemUi = true)
+fun SplashScreen(modifier: Modifier = Modifier) {
+
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Column (
+            modifier = Modifier
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.size(92.dp),
+                painter = painterResource(id = R.drawable.img_splash),
+                contentDescription = "Splash Screen",
+            )
+
+            Spacer(Modifier.height(12.dp))
+
+            Text(
+                text = "Chat with us",
+                fontSize = 20.sp,
+                fontFamily = FontFamily.SansSerif
+            )
+        }
+
+        Text(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 22.dp),
+            text = "Amir work place",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+}
