e1e1cefe0f4765e22caac55c5aa4a139594c3083
amir sohail <amir.sohail.1683@gmail.com>
2025-11-05 14:36:15 +0500

    all add in sharedModule

--- app/src/main/java/com/amar/chat/di/SharedModule.kt
+++ app/src/main/java/com/amar/chat/di/SharedModule.kt
@@ -1,5 +1,9 @@
 package com.amar.chat.di
 
+import com.amar.chat.main_feature.data.repository.FirebaseRepositoryImpl
+import com.amar.chat.main_feature.domain.repository.FirebaseRepository
+import com.amar.chat.main_feature.presentation._view_model.ChatViewModel
+import com.amar.chat.main_feature.presentation._view_model.HomeViewModel
 import com.amar.chat.main_feature.presentation._view_model.PhoneAuthViewModel
 import com.amar.chat.utils.SharedPref
 import com.google.firebase.auth.FirebaseAuth
@@ -13,7 +17,12 @@ val SharedModule = module {
 
     single { FirebaseAuth.getInstance() }
     single { FirebaseDatabase.getInstance() }
+    single { FirebaseDatabase.getInstance().reference }
 
     viewModel { PhoneAuthViewModel(get(), get(), get()) }
+
+    single<FirebaseRepository> { FirebaseRepositoryImpl(get(), get()) }
+    viewModel { HomeViewModel(get()) }
+    viewModel { ChatViewModel(get()) }
 
 }
